1c0ef722c26d8b4b813c72def179edaf3bde53c2
Gautam Srinivasan <gautam.srinivasan7@gmail.com>
2015-02-03 10:49:16 -0800

    Stumplr app folder

---
+++ Stumplr/app/assets/javascripts/stumplr.js
@@ -0,0 +1,13 @@
+window.Stumplr = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Stumplr.initialize();
+});

64988621aa8e9e758b67db4546bfd91c1a1f22b8
Gautam Srinivasan <gautam.srinivasan7@gmail.com>
2015-03-12 17:39:48 -0700

    db files

--- db/migrate/20150216231723_create_likes.rb
+++ db/migrate/20150216231723_create_likes.rb
@@ -1,10 +1,10 @@
-# class CreateLikes < ActiveRecord::Migration
-#   def change
-#     create_table :likes do |t|
-#       t.integer :user_id, null: false
-#       t.integer :post_id, null: false
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
+class CreateLikes < ActiveRecord::Migration
+  def change
+    create_table :likes do |t|
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
